cf92c01cd87f190797ec58399061af88b02707df
Patricia Götz <gotz.pat@gmail.com>
2025-10-19 17:47:36 +0200

    cleaned code

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 data = pd.read_csv("/Users/patriciagoetz/Documents/DataScienceAssignment/luxury_cosmetics_popups.csv")
-#print(data.head(5))
-print("shape", data.shape)
+print("shap before dropping NA", data.shape)
 print(data.isna().sum())
 
 # ger rid of all the rows with missing values
